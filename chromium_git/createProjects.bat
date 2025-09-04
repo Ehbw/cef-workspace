19c77e3c79b8b57c25b8483c3a8c342198ba900c
Ehbw <ehbwwww@gmail.com>
2025-09-04 15:42:30 +0100

    Fix: create projects script

--- chromium_git/createProjects.bat
+++ chromium_git/createProjects.bat
@@ -1,8 +1,13 @@
 @echo off
 
+set ORIGINAL_PATH=%CD%
+
 set GN_DEFINES=is_component_build=true
 set GN_ARGUMENTS=--ide=vs2022 --sln=cef --filters=//cef/*
 
-cd cef
+cd chromium/src/cef
 call cef_create_projects.bat
 cd ..
+
+devenv.exe out/Release_GN_x64/cef.sln
+cd /d "%ORIGINAL_PATH%"
